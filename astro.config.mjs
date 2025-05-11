3448ca02f065c56554c6a532480eabc42edf06bf
Sandon Jurowski <jurowski@gmail.com>
2025-05-11 11:23:01 -0700

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,14 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://sandonjurowski.com',
   base: '/',
-  integrations: [],
+  integrations: [
+    tailwind(),
+    mdx(),
+    sitemap()
+  ],
 });
