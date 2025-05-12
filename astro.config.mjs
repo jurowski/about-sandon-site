e06d41c0bb36269a7dc9784514fcbf26a5b41979
Sandon Jurowski <jurowski@gmail.com>
2025-05-11 19:49:45 -0700

    general updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
@@ -7,7 +6,6 @@ export default defineConfig({
   site: 'https://www.sandonjurowski.com/',
   output: 'static',
   integrations: [
-    tailwind(),
     mdx({ syntaxHighlight: 'shiki', shikiTheme: 'github-dark' }),
     sitemap()
   ],
